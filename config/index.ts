cba7bad99a88ed1437720a668007b0a34e395bb0
wmartinsneto <waldomiro.martins@gmail.com>
2024-10-15 16:36:11 -0300

    Update settings and global styles

--- config/index.ts
+++ config/index.ts
@@ -3,9 +3,9 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'Chat APP',
-  description: '',
-  copyright: '',
+  title: 'IARA - Chatbots IATurbo',
+  description: 'Assistente Super Inteligente dos Chatbots IATurbo',
+  copyright: 'Chatbots IATurbo',
   privacy_policy: '',
   default_language: 'en',
 }
